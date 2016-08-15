0ced8aa30aa0fd72d90b9db3da01cf1b49d8bc7d
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2016-08-15 18:28:27 -0400

    bootable: Add gr_set_font function
    Based off: https://github.com/CyanogenMod/android_bootable_recovery/commit/8e614b89838dda1adff952c0bfbb02721bb5db2b
    
    Change-Id: I595e546235947f693f0b78c84ace4994de2e1858
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- minui/graphics.cpp
+++ minui/graphics.cpp
@@ -80,6 +80,20 @@ void gr_font_size(int *x, int *y)
     *y = gr_font->cheight;
 }
 
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+#endif
+
+typedef struct {
+    char        name[80];
+    GRFont*     font;
+} font_item;
+
+static font_item gr_fonts[] = {
+    { "menu", NULL },
+    { "log", NULL },
+};
+
 void blend_16bpp(unsigned char* px, unsigned r5, unsigned g5, unsigned b5, unsigned char a)
 {
     unsigned char orig[2];
@@ -301,6 +315,17 @@ void gr_fill(int x1, int y1, int x2, int y2)
     }
 }
 
+void gr_set_font(const char* name)
+{
+    unsigned int idx;
+    for (idx = 0; idx < ARRAY_SIZE(gr_fonts); ++idx) {
+        if (strcmp(name, gr_fonts[idx].name) == 0) {
+            gr_font = gr_fonts[idx].font;
+            break;
+        }
+    }
+}
+
 void gr_blit_32to16(GRSurface* source, int sx, int sy, int w, int h, int dx, int dy) {
     dx += overscan_offset_x;
     dy += overscan_offset_y;
@@ -383,11 +408,14 @@ unsigned int gr_get_height(GRSurface* surface) {
     return surface->height;
 }
 
-static void gr_init_font(void)
+static void gr_init_one_font(int idx)
 {
-    gr_font = reinterpret_cast<GRFont*>(calloc(sizeof(*gr_font), 1));
+    char name[80];
+    GRFont* gr_font = reinterpret_cast<GRFont*>(calloc(sizeof(*gr_font), 1));
+    snprintf(name, sizeof(name), "font_%s", gr_fonts[idx].name);
+    gr_fonts[idx].font = gr_font;
 
-    int res = res_create_alpha_surface("font", &(gr_font->texture));
+    int res = res_create_alpha_surface(name, &(gr_font->texture));
     if (res == 0) {
         // The font image should be a 96x2 array of character images.  The
         // columns are the printable ASCII characters 0x20 - 0x7f.  The
@@ -418,6 +446,16 @@ static void gr_init_font(void)
         gr_font->cheight = font.cheight;
     }
 }
+
+static void gr_init_font()
+{
+    unsigned int idx;
+    for (idx = 0; idx < ARRAY_SIZE(gr_fonts); ++idx) {
+        gr_init_one_font(idx);
+    }
+    gr_font = gr_fonts[0].font;
+}
+
 
 #if 0
 // Exercises many of the gr_*() functions; useful for testing.
